4ba10b457dd822201b42b05ea691d2b98d443f8d
ysy <1156546473@qq.com>
2016-07-04 16:26:00 +0800

    require i19n

--- tasks/i19n.js
+++ tasks/i19n.js
@@ -7,7 +7,7 @@
  */
 
 'use strict';
-require('i19n');
+
 
 module.exports = function(grunt) {
 
@@ -16,36 +16,12 @@ module.exports = function(grunt) {
 
   grunt.registerMultiTask('i19n', 'The best Grunt plugin ever.', function() {
     // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
+    var src = this.data.src;
+    var dest = this.data.dest;
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+    var lang = grunt.file.expand(src);
 
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
+    require('i19n')(lang, dest);
   });
 
 };
